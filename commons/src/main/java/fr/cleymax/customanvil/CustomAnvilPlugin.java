54c81e5460fb6db4ab938199c212347c6c0a1849
Clément P. (Cleymax) <clemperrin1@gmail.com>
2019-08-27 00:56:39 +0200

    :tada: Initial Commit

---
+++ commons/src/main/java/fr/cleymax/customanvil/CustomAnvilPlugin.java
@@ -0,0 +1,23 @@
+package fr.cleymax.customanvil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * File <b>CustomAnvilPlugin</b> located on fr.cleymax.customanvil
+ * CustomAnvilPlugin is a part of CustomAnvil.
+ * <p>
+ * Copyright (c) 2019 CustomAnvil .
+ * <p>
+ *
+ * @author Clément P. (Cleymax), {@literal <cleymaxpro@gmail.com>}
+ * Created the 27/08/2019 at 00:41
+ */
+
+public final class CustomAnvilPlugin extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+	
+	}
+}
